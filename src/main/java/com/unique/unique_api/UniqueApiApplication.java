1d0883c22ce8c4ddd9e756327ded600107c6e451
Senait Ghebrehiwet <79334266+senigheb@users.noreply.github.com>
2022-07-14 12:39:18 -0400

    Add files via upload

---
+++ src/main/java/com/unique/unique_api/UniqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.unique.unique_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unique/unique_api/UniqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unique.unique_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
