18cf40859a185c065292590f6c0ae96e27c2c005
RuthT <87043497+BlaBlue1@users.noreply.github.com>
2022-07-15 17:21:13 -0400

    Delete UserRepository.java

--- src/main/java/com/unique/unique_api/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unique.unique_api.repositories;
-
-import com.unique.unique_api.repositories.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-//    User findByName(String username);
-    User findByUsername(String username);
-}
